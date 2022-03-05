2d5f2e05eb59bd7fbfe6b98d37a7597e5d4f95d0
Gandolphinnn <gandolfiluca03@gmail.com>
2022-03-05 19:12:42 +0100

    working rays w/o walls

--- raycasting.js
+++ raycasting.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 	function convertAngle(angle, format) {
 		if (format == 'rad') {
-			//todo convert to radiant
+			angle = angle * Math.PI / 180;
 		}
 		else if (format == 'degr') {
 			//todo convert to degree
@@ -20,6 +20,7 @@ window.onload = function() {
 			this.p2 = p2;
 		}
 		draw() {
+			ctx.beginPath();
 			ctx.moveTo(this.p1.x, this.p1.y);
 			ctx.lineTo(this.p2.x, this.p2.y);
 			ctx.stroke();
@@ -28,48 +29,69 @@ window.onload = function() {
 	class Ray {
 		constructor(degr) {
 			this.degr = degr;
+			this.rad = convertAngle(this.degr, 'rad');
 			this.endP = new Point();
-			this.length = 0;
 		}
 		calculateEnd() {
-			let rad = convertAngle(this.degr, 'rad');
-			this.endP.x = 100;
-			this.endP.y = 200;
-			return this.endP;
+			//(this.degr > 90 && this.degr < 270)?;
+			let sin = Math.sin(this.rad);
+			let cos = Math.cos(this.rad);
+			this.endP.x = origin.o.x + cos;
+			this.endP.y = origin.o.y - sin;
+			do {
+				this.endP.x += cos * 0.1;
+				this.endP.y -= sin * 0.1;
+			} while (!(this.endP.x < -5 || this.endP.x > cvs.x + 5 || this.endP.y < -5 || this.endP.y > cvs.y + 5));
+			this.length = Math.sqrt((origin.o.x - this.endP.x) ** 2 + (origin.o.y - this.y) ** 2);
+			//* https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+			//* Given_two_points_on_each_line_segment
+		}
+		draw() {
+			ctx.beginPath();
+			ctx.moveTo(origin.o.x, origin.o.y);
+			ctx.lineTo(this.endP.x, this.endP.y);
+			ctx.stroke();
 		}
 	}
 	class Origin {
 		constructor(p) {
 			this.o = p;
 			this.rays = new Array();
-			for (let i = 0; i < 1; i++) {
+			for (let i = 0; i < 360; i++) {
 				this.rays.push(new Ray(i));
+				this.rays.push(new Ray(i+0.25));
+				this.rays.push(new Ray(i+0.5));
+				this.rays.push(new Ray(i+0.75));
 			}
 		}
 		draw() {
+			this.rays.forEach(ray => {
+				ray.draw();
+			});
 			ctx.beginPath();
 			ctx.arc(this.o.x, this.o.y, 10, 0, 2 * Math.PI);
 			ctx.fill();
-			ctx.stroke();
-			this.rays.forEach(ray => {
-				let endP = ray.calculateEnd();
-				ctx.moveTo(this.o.x, this.o.y);
-				ctx.lineTo(endP.x, endP.y);
-				ctx.stroke();
-			});
 		}
 	}
 	function animate() {
 		requestAnimationFrame(animate);
 		ctx.clearRect(0,0, innerWidth, innerHeight);
+		//walls.forEach(wall => { wall.draw() });
 		origin.draw();
 	}
 	let canvas = document.querySelector("canvas");
 	let ctx = canvas.getContext("2d");
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-	ctx.fillStyle = 'white';
-	ctx.strokeStyle = 'white';
+	ctx.fillStyle = 'red';
+	ctx.strokeStyle = 'gray';
+	ctx.lineWidth = '3';
+	let cvs = { x: canvas.width, y: canvas.height };
+	let walls = new Array();
+	walls.push(new Wall(new Point(400, 200), new Point(500, 300)))
 	let origin = new Origin(new Point(canvas.width/2, canvas.height/2));
+	origin.rays.forEach(ray => {
+		ray.calculateEnd();
+	});
 	animate();
 }
