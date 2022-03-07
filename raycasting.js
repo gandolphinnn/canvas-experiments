f08801fa32d12a9af5d5368c73e3f343603526db
gandnolphinnn <gandolfiluca03@gmail.com>
2022-03-07 11:00:37 +0100

    algorithm for collision

--- raycasting.js
+++ raycasting.js
@@ -1,18 +1,25 @@
 window.onload = function() {
-	function convertAngle(angle, format) {
-		if (format == 'rad') {
-			angle = angle * Math.PI / 180;
+	document.addEventListener('mousedown', function(e) {
+		if (e.button == 0) {
+			mouse.state = true;
 		}
-		else if (format == 'degr') {
-			//todo convert to degree
-		}
-		return angle;
-	}
+	});
+	document.addEventListener('mouseup', function(e) {
+		mouse.state = false;
+	});
+	document.addEventListener('mousemove', function(e) {
+		mouse.pos.y = e.clientY;
+		mouse.pos.x = e.clientX;
+	});
 	class Point {
 		constructor(x, y) {
 			this.x = x;
 			this.y = y;
 		}
+	}
+	let mouse = {
+		pos: new Point(null, null),
+		state: false
 	}
 	class Wall {
 		constructor(p1, p2) {
@@ -29,22 +36,34 @@ window.onload = function() {
 	class Ray {
 		constructor(degr) {
 			this.degr = degr;
-			this.rad = convertAngle(this.degr, 'rad');
+			this.rad = this.degr * Math.PI / 180;
 			this.endP = new Point();
 		}
 		calculateEnd() {
-			//(this.degr > 90 && this.degr < 270)?;
 			let sin = Math.sin(this.rad);
 			let cos = Math.cos(this.rad);
-			this.endP.x = origin.o.x + cos;
-			this.endP.y = origin.o.y - sin;
+			let O = origin.o;
+			this.endP.x = O.x + cos;
+			this.endP.y = O.y - sin;
 			do {
 				this.endP.x += cos * 0.1;
 				this.endP.y -= sin * 0.1;
 			} while (!(this.endP.x < -5 || this.endP.x > cvs.x + 5 || this.endP.y < -5 || this.endP.y > cvs.y + 5));
-			this.length = Math.sqrt((origin.o.x - this.endP.x) ** 2 + (origin.o.y - this.y) ** 2);
+			this.length = Math.sqrt((O.x - this.endP.x) ** 2 + (O.y - this.y) ** 2);
 			//* https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
 			//* Given_two_points_on_each_line_segment
+			walls.forEach(wall => {
+				let x1 = O.x, x2 = this.endP.x, x3 = wall.p1.x, x4 = wall.p2.x;
+				let y1 = O.y, y2 = this.endP.y, y3 = wall.p1.y, y4 = wall.p2.y;
+				let denom = (x1-x2)*(y3-y4) - (x3-x4)*(y1-y2);
+				let t = ((x1-x3)*(y3-y4) - (x3-x4)*(y1-y3)) / denom;
+				let u = ((x1-x3)*(y1-y2) - (x1-x2)*(y1-y3)) / denom;
+				if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+					this.endP.x = x1 + t * (x2 - x1);
+					this.endP.y = y1 + t * (y2 - y1);
+				}
+			});
+
 		}
 		draw() {
 			ctx.beginPath();
@@ -59,9 +78,9 @@ window.onload = function() {
 			this.rays = new Array();
 			for (let i = 0; i < 360; i++) {
 				this.rays.push(new Ray(i));
-				this.rays.push(new Ray(i+0.25));
-				this.rays.push(new Ray(i+0.5));
-				this.rays.push(new Ray(i+0.75));
+				//this.rays.push(new Ray(i+0.25));
+				//this.rays.push(new Ray(i+0.5));
+				//this.rays.push(new Ray(i+0.75));
 			}
 		}
 		draw() {
@@ -72,11 +91,17 @@ window.onload = function() {
 			ctx.arc(this.o.x, this.o.y, 10, 0, 2 * Math.PI);
 			ctx.fill();
 		}
+		move() {
+			console.log(mouse.state); // !bug
+			if (mouse.state) {
+				this.o = mouse.pos;
+			}
+		}
 	}
 	function animate() {
 		requestAnimationFrame(animate);
 		ctx.clearRect(0,0, innerWidth, innerHeight);
-		//walls.forEach(wall => { wall.draw() });
+		walls.forEach(wall => { wall.draw() });
 		origin.draw();
 	}
 	let canvas = document.querySelector("canvas");
@@ -88,7 +113,8 @@ window.onload = function() {
 	ctx.lineWidth = '3';
 	let cvs = { x: canvas.width, y: canvas.height };
 	let walls = new Array();
-	walls.push(new Wall(new Point(400, 200), new Point(500, 300)))
+	//walls.push(new Wall(new Point(400, 200), new Point(200, 400)))
+	walls.push(new Wall(new Point(800, 200), new Point(200, 600)))
 	let origin = new Origin(new Point(canvas.width/2, canvas.height/2));
 	origin.rays.forEach(ray => {
 		ray.calculateEnd();
